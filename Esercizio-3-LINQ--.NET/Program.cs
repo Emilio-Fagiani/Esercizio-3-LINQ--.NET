e0a25cec71f9e37388f2e1c1ae3372d4156f5229
emifa <emifa@LAPTOP-1TM449QB>
2023-07-05 13:59:56 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-3-LINQ--.NET/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Esercizio_3_LINQ__.NET;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        RandomNumber rm = new RandomNumber();
+        List<int> randomIntegers = rm.GenerateRandomNum(5);
+
+        Console.WriteLine("Sort");
+        randomIntegers.Sort();
+
+         foreach (int num in randomIntegers)
+        {
+            Console.WriteLine(num);
+        }
+        Console.WriteLine("______");
+        Console.WriteLine();
+        Console.WriteLine("Desc");
+
+        randomIntegers.Reverse();
+
+        foreach (int num in randomIntegers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Esercizio-3-LINQ--.NET/RandomNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_3_LINQ__.NET
+{
+    public class RandomNumber
+    {
+        public List<int> GenerateRandomNum(int num)
+        {
+            Random random = new Random();
+            List<int> listInt = new List<int>();
+            for (int i = 0; i < num; i++)
+            {
+                int randomNum = random.Next();
+                listInt.Add(randomNum);
+               // Console.WriteLine(listInt[i]);
+            }
+            return listInt;
+        }
+    }
+}
